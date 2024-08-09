3743871b0eeb51dfebab0a60ce2796061f930ac0
titumiranan123 <titumiranan.gtc@gmail.com>
2024-08-09 10:32:34 -0600

    update pricing

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -8,31 +8,23 @@ const Pricing: React.FC = () => {
                 <h1 className='text-[56px] bold text-center  text-white'>Pricing Plans</h1>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 mt-[80px] lg:grid-cols-3 gap-5'>
-                <div className='optioncard  bg-[#0E141E] md:w-[386px] w-[98%] group h-[714px] transition-colors duration-200  rounded-[24px] group'>
+                <div className='optioncard  bg-[#0E141E] md:w-[386px] w-[98%] group h-[744px] transition-colors duration-200  rounded-[24px] group'>
                     <div className='  flex justify-center items-center'>
                         <p className='bg-[#25AAE1] hover: w-[162px] h-[48px] flex justify-center items-center rounded-br-[8px] rounded-bl-[8px] group-hover:bg-[#0E141E] text-white normal text-[20px]'>Basic</p>
                     </div>
                     <div className='group-hover:text-[#03070D] text-white'>
-                        <h1 className='bold text-[48px]    text-center mt-9  leading-[60px]'>$40</h1>
-                        <p className=' normal text-center leading-[25.6px] ms-4 -mt-2 text-[20px]'>15 Minutes</p>
+                        <h1 className='bold text-[48px]    text-center mt-9  leading-[60px]'>$30</h1>
+                        <p className=' normal text-center leading-[25.6px] ms-4 -mt-2 text-[20px]'>Up To 60 Seconds</p>
                     </div>
                     <div className='mt-[44px] ps-[36px]  flex flex-col gap-[14px] '>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
 
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>4 Day Delivery</p>
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>3 Day Delivery</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>2 Day Revisions</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Up to 15 minutes of footage provided</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Up to 15 minutes running time</p>
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>2 Revisions</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
@@ -40,17 +32,32 @@ const Pricing: React.FC = () => {
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Sound design & mixing</p>
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Subtitles Included</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Motion Graphics</p>
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Smooth Transition </p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Thumbnail</p>
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>B-roll</p>
                         </div>
-
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Enhanced Audio </p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px] group-hover:text-[#0E141E] text-white'>Sound design & mixing </p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  text-[#45555B]' />
+                            <p className=' regular leading-[25.6px] text-[20px] text-[#45555B] '>Motion Graphics </p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  text-[#45555B]' />
+                            <p className=' regular leading-[25.6px] text-[20px] text-[#45555B] '>Thumnail </p>
+                        </div>
                     </div>
                     <div className='flex justify-center items-center mt-[44px] mb-[56px]'>
                         <button className='bg-[#25AAE1] py-4 px-6  group-hover:bg-[#0E141E] text-white  rounded-[16px] normal'> Get Started</button>
@@ -58,13 +65,13 @@ const Pricing: React.FC = () => {
 
                 </div>
                 {/* second  */}
-                <div className='optioncard  bg-[#0E141E] md:w-[386px] w-[98%] group h-[714px] transition-colors duration-200  rounded-[24px]'>
+                <div className='optioncard  bg-[#0E141E] md:w-[386px] w-[98%] group h-[724px] transition-colors duration-200  rounded-[24px]'>
                     <div className='  flex justify-center items-center'>
                         <p className='bg-[#25AAE1] hover: w-[162px] h-[48px] flex justify-center items-center rounded-br-[8px] rounded-bl-[8px] group-hover:bg-[#0E141E]  text-white normal text-[20px]'>Standard</p>
                     </div>
                     <div className='group-hover:text-[#03070D] text-white'>
-                        <h1 className='bold text-[48px]    text-center mt-9  leading-[60px]'>$100</h1>
-                        <p className=' normal text-center leading-[25.6px] ms-4 -mt-2 text-[20px]'>15 Minutes</p>
+                        <h1 className='bold text-[48px]    text-center mt-9  leading-[60px]'>$45</h1>
+                        <p className=' normal text-center leading-[25.6px] ms-4 -mt-2 text-[20px]'>Up To 60 Seconds</p>
                     </div>
                     <div className='mt-[44px] ps-[36px] flex flex-col gap-[14px] group-hover:text-[#03070D] text-white'>
                         <div className='flex gap-2'>
@@ -73,19 +80,23 @@ const Pricing: React.FC = () => {
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px]'>3 Day Revisions</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px]'>Up to 45 minutes of footage provided</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px]'>Up to 45 minutes running time</p>
+                            <p className=' regular leading-[25.6px] text-[20px]'>3 Revisions</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
                             <p className=' regular leading-[25.6px] text-[20px]'>Color Grading</p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px]'>Subtitles Included</p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px]'>Smooth Transition</p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px]'>Enhanced Audio</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
@@ -99,7 +110,6 @@ const Pricing: React.FC = () => {
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
                             <p className=' regular leading-[25.6px] text-[20px]'>Thumbnail</p>
                         </div>
-
                     </div>
                     <div className='flex justify-center items-center mt-[44px]  '>
                         <button className='bg-[#25AAE1] py-4 px-6 group-hover:bg-[#0E141E]  rounded-[16px] normal  text-white mb-[56px]'> Get Started</button>
@@ -107,13 +117,17 @@ const Pricing: React.FC = () => {
 
                 </div>
                 {/* third  */}
-                <div className='optioncard  bg-[#0E141E] md:w-[386px] w-[98%] group h-[714px] transition-colors duration-200  rounded-[24px]'>
+                <div className='optioncard  bg-[#0E141E] md:w-[386px] w-[98%] group h-[780px] transition-colors duration-200  rounded-[24px]'>
                     <div className='  flex justify-center items-center text-white'>
                         <p className='bg-[#25AAE1] hover: w-[162px] h-[48px] flex justify-center items-center rounded-br-[8px] rounded-bl-[8px] group-hover:bg-[#0E141E]  normal text-[20px]'>Premium</p>
                     </div>
-                    <div className='group-hover:text-[#03070D] text-white '>
-                        <h1 className='bold text-[48px]    text-center mt-9  leading-[60px]'>$130</h1>
-                        <p className=' normal text-center leading-[25.6px] ms-4 -mt-2 text-[20px]'>15 Minutes</p>
+                    <div className='group-hover:text-[#03070D] text-white'>
+                        <h1 className='bold text-[48px]    text-center mt-9  leading-[60px]'>$70</h1>
+                        <p className=' normal text-center leading-[25.6px] ms-4 -mt-2 text-[20px]'>Up To 60 Seconds</p>
+                    </div>
+                    <div>
+
+                        <p className='group-hover:text-[#03070D] text-white mt-[20px] md:text-[20px] md:leading-[25.3px] text-[14px] leading-[18.3px] px-4'>VIP services (Advance Quality like Iman gadzi, ali adbaal, alex hormozi)</p>
                     </div>
                     <div className='mt-[44px] ps-[36px] flex flex-col gap-[14px] group-hover:text-[#03070D] text-white'>
                         <div className='flex gap-2'>
@@ -122,19 +136,23 @@ const Pricing: React.FC = () => {
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px]'>3 Day Revisions</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px]'>Up to 60 minutes of footage provided</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
-                            <p className=' regular leading-[25.6px] text-[20px]'>Up to 60 minutes running time</p>
+                            <p className=' regular leading-[25.6px] text-[20px]'>3 Revisions</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
                             <p className=' regular leading-[25.6px] text-[20px]'>Color Grading</p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px]'>Subtitles Included</p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px]'>Smooth Transition</p>
+                        </div>
+                        <div className='flex gap-2'>
+                            <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
+                            <p className=' regular leading-[25.6px] text-[20px]'>Enhanced Audio</p>
                         </div>
                         <div className='flex gap-2'>
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
@@ -148,7 +166,6 @@ const Pricing: React.FC = () => {
                             <LuCheckCircle2 className='w-6 h-6  group-hover:text-[#0E141E] text-white' />
                             <p className=' regular leading-[25.6px] text-[20px]'>Thumbnail</p>
                         </div>
-
                     </div>
                     <div className='flex justify-center items-center mt-[44px] mb-[56px]'>
                         <button className='bg-[#25AAE1] py-4 px-6 group-hover:bg-[#0E141E]  rounded-[16px] normal text-white'> Get Started</button>
